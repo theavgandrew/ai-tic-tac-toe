1b06a540188846bd9f98575d38e68db02fc2710c
Andrew Dang <63373017+theavgandrew@users.noreply.github.com>
2025-01-09 17:27:43 -0600

    allows the ai and human to make a move

--- AITicTacToe.py
+++ AITicTacToe.py
@@ -2,15 +2,30 @@ from math import inf as infinity
 from random import choice 
 
 # game board 
-board = [[0, 0, 0], 
-         [0, 0, 0],
-         [0, 0, 0]]
+board = [
+  [0, 0, 0], 
+  [0, 0, 0],
+  [0, 0, 0],
+] 
 
 AI = +1
 HUMAN = -1
 
-def ai_move():
-  print
+
+'''
+function sets the move on the board given the specifed x and y coordinates and player
+:param x: x coordinate on the board
+:param y: y coordinate on the board
+:param player: specified player whose move the function is setting
+:return: True if setting the move was successful; otherwise, False
+'''
+def set_move(x, y, player):
+  if board[x][y] == 0: # if cell at [x, y] is empty
+    board[x][y] = player
+    return True
+  else: 
+    return False
+
 
 '''
 function used to determine the static value of the state
@@ -48,10 +63,10 @@ def minimax(state, depth, player):
 
     if player == AI: 
       if curr_val[2] > best_val[2]: # ensures max value 
-        best_val[2] = curr_val[2]
+        best_val = curr_val
     elif player == HUMAN: 
       if curr_val[2] < best_val[2]: # ensures min value
-        best_val[2] = curr_val[2]
+        best_val = curr_val
 
   return best_val
 
@@ -69,7 +84,7 @@ def empty_cells(state):
 Checks to see if a specific player has won the game in the given state of the game
 :param state: the given state of the game 
 :param player: states whether the specified player is the human or AI
-:return: True if the specified player wins the game; otherwise, false
+:return: True if the specified player wins the game; otherwise, False
 '''
 def check_win(state, player): 
   possible_wins = [[state[0][0], state[0][1], state[0][2]], 
@@ -84,15 +99,84 @@ def check_win(state, player):
     return True
   else: 
     return False 
+  
+def ai_move(c_symb, h_symb):
+  depth = len(empty_cells(board))
+  if depth == 0 or game_over(board): 
+    return 
+  
+  clear()
+  print(f"It's the AI's turn [{c_symb}]") 
+  print_board(board, c_symb, h_symb)
+
+  if depth == 9:
+    x = choice([0, 1, 2])
+    y = choice([0, 1, 2]) 
+  else:
+    move = minimax(board, depth, AI)
+    x, y = move[0], move[1]
+
+  return set_move(x, y, AI)
+
+def human_move(c_symb, h_symb): 
+  depth = len(empty_cells(board))
+  if depth == 0 or game_over(board): 
+    return 
+  
+  # valid moves
+  move = -1
+  possible_moves = {
+    1: [0, 0], 2: [0, 1], 3: [0, 2], 
+    4: [1, 0], 5: [1, 1], 6: [1, 2], 
+    7: [2, 0], 8: [2, 1], 9: [2, 2], 
+  }
+
+  clear()
+  print(f"It's your turn [{h_symb}]")
+  print_board(c_symb, h_symb)
+
+  while not 1 > move > 9: 
+    try:
+      move = int(input("Input your move (1-9): "))
+      coord = possible_moves[move] 
+      can_move = set_move(coord[0], coord[1], HUMAN)
+
+      if not can_move: 
+        print("Invalid nove. Try again.") 
+        move = -1
+    except (EOFError, KeyboardInterrupt):
+      # if user interrupts the program
+      print("Good game! Bye")
+      exit()
+    except (KeyError, ValueError): 
+      # if user inputs something that's not in dictionary
+      # or not a number
+      print("Horribly invalid move. Try again.")
+
+
+
+
+
+
+def clear():
+  print()
+def print_board(state, c_symb, h_symb): 
+  print()
 
 '''
 Determines whether the game is over or not
 :param state: the given state of the game
-:return: True if someone has wins the game; otherwise, false
+:return: True if someone has wins the game; otherwise, False
 '''
 def game_over(state):
   return check_win(state, HUMAN) or check_win(state, AI) 
 
 def player_move():
   print()
+
+def play_game():
+  print
+
+if __name__ == '__main__': 
+  play_game()
 
